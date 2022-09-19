32bc549763f4b6877c032847faecf6a378970e7d
Chris Hood <chris.hood@versent.com.au>
2022-09-20 09:11:26 +1000

    adding sg

--- main.tf
+++ main.tf
@@ -18,3 +18,28 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 }
+
+resource "aws_security_group" "test_sg" {
+  name        = "cjhood_testsg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = vpc-0742f34632726d631
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["172.31.0.0/16"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cjhood_test"
+  }
+}
